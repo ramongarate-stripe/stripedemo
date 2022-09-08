115bc58cfca055cf289311dc8282a7195bceffd9
Ramon Garate <ramongarate@stripe.com>
2022-09-08 16:22:21 +0200

    wip payment method screen

--- PaymentMethodsScreen.js
+++ PaymentMethodsScreen.js
@@ -1,12 +1,30 @@
-import { TouchableOpacity, Text, View, StyleSheet } from "react-native";
+import {
+  TouchableOpacity,
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  Image,
+  StatusBar,
+  Alert,
+} from "react-native";
 import { useState, useContext, useEffect } from "react";
 import { Context } from "./Context";
 import { API_URL } from "./constants";
+
+const capitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+
+const CARD_DICT = {
+  amex: require("./assets/amex.png"),
+  mastercard: require("./assets/mastercard.png"),
+  visa: require("./assets/visa.png"),
+};
 
 export default function PaymentMethodsScreen() {
   const { customer, setCustomer } = useContext(Context);
   const [loading, setLoading] = useState(false);
   const [paymentMethods, setPaymentMethods] = useState(null);
+
   const login = async () => {
     setLoading(true);
     const response = await fetch(`${API_URL}/login`, {
@@ -22,6 +40,7 @@ export default function PaymentMethodsScreen() {
   };
   const logout = () => {
     setCustomer(null);
+    setPaymentMethods(null);
   };
 
   useEffect(() => {
@@ -42,6 +61,48 @@ export default function PaymentMethodsScreen() {
     }
   }, [customer]);
 
+  const showAlert = (item) => {
+    Alert.alert(capitalize(item.brand), "What would you like to do?", [
+      {
+        text: "Set as Default",
+        onPress: () =>
+          Alert.alert(
+            `${capitalize(item.brand)} ending in ${
+              item.last4
+            } is now the default payment method`
+          ),
+      },
+      {
+        text: "Delete Card",
+        onPress: () =>
+          Alert.alert(
+            `${capitalize(item.brand)} ending in ${item.last4} deleted`
+          ),
+        style: "destructive",
+      },
+      {
+        text: "Cancel",
+        style: "cancel",
+      },
+    ]);
+  };
+
+  const Item = ({ item }) => (
+    <TouchableOpacity style={styles.item} onPress={() => showAlert(item)}>
+      <Image style={styles.image} source={CARD_DICT[item.brand]} />
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{capitalize(item.brand)}</Text>
+        <Text style={styles.last4}>Ending in {item.last4}</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Text style={styles.default}>Default</Text>
+        <Image style={styles.menu} source={require("./assets/menu.png")} />
+      </View>
+    </TouchableOpacity>
+  );
+
+  const renderItem = ({ item }) => <Item item={item} />;
+
   return (
     <View>
       <Text>
@@ -49,6 +110,11 @@ export default function PaymentMethodsScreen() {
           ? "Hello " + customer.name
           : "Please click the button to Log In"}
       </Text>
+      <FlatList
+        data={paymentMethods}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.fingerprint}
+      />
       {!customer ? (
         <TouchableOpacity
           style={styles.loginButton}
@@ -87,5 +153,75 @@ const styles = StyleSheet.create({
   loginText: {
     fontSize: 20,
     color: "white",
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 10,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 10,
+    flexDirection: "row",
+  },
+  image: {
+    width: 64,
+    height: 36,
+    borderRadius: 5,
+  },
+  menu: {
+    width: 30,
+    height: 20,
+  },
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  removeButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 40,
+    height: 40,
+  },
+  totalAmount: {
+    fontSize: 20,
+    margin: 20,
+  },
+  checkoutButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 200,
+    height: 50,
+    backgroundColor: "black",
+    borderRadius: 5,
+    marginBottom: 50,
+  },
+  buttonText: {
+    fontSize: 40,
+    color: "white",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  price: {
+    fontSize: 15,
+    paddingTop: 5,
+  },
+  quantity: {
+    padding: 10,
+    fontSize: 20,
+  },
+  textContainer: {
+    flexDirection: "column",
+    width: 140,
+    alignItems: "left",
+    paddingLeft: 10,
+    borderRadius: 5,
+    height: 50,
   },
 });
