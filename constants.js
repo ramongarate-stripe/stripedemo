5f85a779f16a092cc4e0fdc0d87b5b5a63647c8d
Ramon Garate <ramongarate@stripe.com>
2022-09-27 11:05:27 +0200

    add furniture pngs

--- constants.js
+++ constants.js
@@ -8,18 +8,18 @@ export const DATA = [
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
     title: "Sofa",
     price: 1000,
-    imageUrl: "https://reactnative.dev/img/tiny_logo.png",
+    imageUrl: "https://i.ibb.co/H2J3mNR/sofa.png",
   },
   {
     id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
     title: "Chair",
     price: 100,
-    imageUrl: "https://reactnative.dev/img/tiny_logo.png",
+    imageUrl: "https://i.ibb.co/rydHjsK/chair.png",
   },
   {
     id: "58694a0f-3da1-471f-bd96-145571e29d72",
     title: "Desk",
     price: 500,
-    imageUrl: "https://reactnative.dev/img/tiny_logo.png",
+    imageUrl: "https://i.ibb.co/LgxS1nZ/desk.png",
   },
 ];
