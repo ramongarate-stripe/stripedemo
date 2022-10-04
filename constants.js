a65e02c616287138113aca35bc6d2501cf2476c5
Ramon Garate <ramongarate@stripe.com>
2022-10-04 14:42:02 +0200

    add placeholders

--- constants.js
+++ constants.js
@@ -1,8 +1,6 @@
 // constants.js
-export const API_URL =
-  "https://stripe-react-native-sample-app-backend.glitch.me";
-export const PUBLISHABLE_KEY =
-  "pk_test_51KH3VNGoGfNrsrt2sb0IpXS8BqK3U9gSrth2zVf0jY3JUWnxtHdFVuNQvVoAp3WVn5aEEo5wUcbBMt9uZHkdAoyA00O5rwrGom";
+export const API_URL = "{{API_URL}}";
+export const PUBLISHABLE_KEY = "{{PUBLISHABLE_KEY}}";
 
 export const DATA = [
   {
